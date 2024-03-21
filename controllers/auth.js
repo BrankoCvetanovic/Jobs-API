008e5be9b4509c73aca9eb84ba569a94c1d84189
BahkoHatake <cbranko01@gmail.com>
2024-03-21 13:59:02 +0100

    Add password hash

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,17 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
+const bcrypt = require("bcryptjs");
+
 async function register(req, res) {
-  res.send("register user");
+  const { name, email, password } = req.body;
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const tempUser = { name, email, password: hashedPassword };
+  const user = await User.create({ ...tempUser });
+  res.status(StatusCodes.CREATED).json({ user });
 }
 async function login(req, res) {
   res.send("login user");
